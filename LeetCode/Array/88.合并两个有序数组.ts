0615fa12a1d81eda8a635b988f5d5a719801abe6
taoyi <tao.yi@treelab.co>
2022-02-13 15:30:02 +0800

    合并两个有序数组

---
+++ LeetCode/Array/88.合并两个有序数组.ts
@@ -0,0 +1,42 @@
+/*
+给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+
+请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+
+注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+
+
+*/
+
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+
+/**
+ [1,2,3,0,0,0]   m = 3
+ [2,5,6]         n = 3
+ 
+ tail = m + n - 1 末尾
+ i = m - 1
+ j = n - 1
+
+ 双指针：从后往前比较两个数组i和j位置处最大的数，依次放入末尾
+*/
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  let tail = m + n - 1;
+  let i = m - 1;
+  let j = n - 1;
+
+  while (i >= 0 || j >= 0) {
+    if (i < 0) {
+      nums1[tail] = nums2[j--];
+    } else if (j < 0) {
+      nums1[tail] = nums1[i--];
+    } else if (nums1[i] > nums2[j]) {
+      nums1[tail] = nums1[i--];
+    } else {
+      nums1[tail] = nums2[j--];
+    }
+    tail--;
+  }
+}
